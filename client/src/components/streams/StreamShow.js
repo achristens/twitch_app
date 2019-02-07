1b620cd82c02100b4db6aa7a346fd8726cf995f1
Abby Christens <achristens@gmail.com>
2019-02-07 12:13:54 -0500

    Hooking up flv.js video player to connect to OBS in StreamShow component

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount(){
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render(){
@@ -12,10 +39,12 @@ class StreamShow extends Component {
       return <div>Loading...</div>
     }
 
-    const { title, description } = this.props.stream; //this destructuring helps to avoid 'this.props.stream' repetition in the JSX below
+    const { title, description } = this.props.stream; // This destructuring helps to avoid 'this.props.stream' repetition in the JSX below
 
+    // BELOW:  writing "controls" is the same as writing controls={true}, this only applies to true not false
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
